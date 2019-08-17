a46a4d2c7dfbfc76a2bc9755c7088a6e7fef1363
mizoguchi kenta <347lionz@gmail.com>
2019-08-17 23:01:19 +0900

    Initial Commit

---
+++ PachiSimulator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PachiSimulator
+//
+//  Created by 溝口健太 on 2019/08/17.
+//  Copyright © 2019 溝口健太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
